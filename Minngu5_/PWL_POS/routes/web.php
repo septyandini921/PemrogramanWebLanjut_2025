083e27973d7a76fe2f7d80cad54852b3ae69b2ea
NimasSeptiandini <septyandini921@gmail.com>
2025-03-17 13:36:23 +0700

    Praktikum 3

--- Minngu5_/PWL_POS/routes/web.php
+++ Minngu5_/PWL_POS/routes/web.php
@@ -33,4 +33,8 @@ Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
+//Jobsheet 5
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
