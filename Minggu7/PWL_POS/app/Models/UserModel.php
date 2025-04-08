ca4db9f322bd274cd3b962fbd1cd0fae67ce879a
NimasSeptiandini <septyandini921@gmail.com>
2025-04-08 18:44:55 +0700

    Tugas 1

--- Minggu7/PWL_POS/app/Models/UserModel.php
+++ Minggu7/PWL_POS/app/Models/UserModel.php
@@ -7,18 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
-
-    protected $table = 'm_user'; //mendifinisikan nama tabel yang digunakan oleh model
-    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
-   
-    protected $hidden = ['password']; //jangan ditampilkan saat select
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
-    protected $casts = ['password' => 'hashed']; //casting password agar otomatis di hash
-
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
