172c5ae6709bc3f47ff50c87cde0637082b7032f
NimasSeptiandini <septyandini921@gmail.com>
2025-04-08 16:14:16 +0700

    Praktikum 1

--- Minggu7/PWL_POS/app/Models/UserModel.php
+++ Minggu7/PWL_POS/app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
 class UserModel extends Model
 {
@@ -12,8 +13,11 @@ class UserModel extends Model
 
     protected $table = 'm_user'; //mendifinisikan nama tabel yang digunakan oleh model
     protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+   
+    protected $hidden = ['password']; //jangan ditampilkan saat select
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $casts = ['password' => 'hashed']; //casting password agar otomatis di hash
 
     public function level(): BelongsTo
     {
--- Minggu7/PWL_POS/routes/web.php
+++ Minggu7/PWL_POS/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -146,4 +147,17 @@ Route::group(['prefix' => 'barang'], function () {
     Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
 
     Route::get('/{id}/confirm_delete', [BarangController::class, 'confirm_ajax'])->name('barang.confirm_delete');
+});
+
+//W7
+Route::pattern('id','[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+
+Route::middleware(['auth'])->group(function(){ // artinya semua route di dalam group ini harus login dulu
+
+    // masukkan semua route yang perlu autentikasi di sini
+
 });
