f5b3c7e6befed2d18f9ba11660ba57583d7c4b23
NimasSeptiandini <septyandini921@gmail.com>
2025-04-13 02:07:31 +0700

    Praktikum 2: export

--- Minggu8/PWL_POS/routes/web.php
+++ Minggu8/PWL_POS/routes/web.php
@@ -120,6 +120,7 @@ Route::middleware(['authorize:ADM,MNG,STF'])->prefix('barang')->group(function (
     Route::delete('/{id}', [BarangController::class, 'destroy'])->name('barang.destroy');
     Route::get('/import', [BarangController::class, 'import']);
     Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+    Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
 });
 
 });
